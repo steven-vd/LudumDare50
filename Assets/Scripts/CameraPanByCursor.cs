49397639da963114275865f1d59c73292c11a9bf
Steven VD <steven@vandorp.lu>
2022-04-02 15:10:21 +0200

    Added CameraPanByCursor and TextMeshPro

---
+++ Assets/Scripts/CameraPanByCursor.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class CameraPanByCursor : MonoBehaviour {
+
+    public float minY = 540.0f, maxY;
+    public float edgePanDistance;
+    public float panSpeed = 1;
+
+    [HideInInspector]
+    public float desiredY;
+
+    void Update() {
+        if (Input.mousePosition.y > Screen.height - edgePanDistance) {
+            desiredY = maxY;
+        } else if (Input.mousePosition.y < edgePanDistance) {
+            desiredY = minY;
+        }
+
+        transform.position = new Vector3(
+            transform.position.x,
+            Mathf.Lerp(transform.position.y, desiredY, panSpeed * Time.deltaTime),
+            transform.position.z
+        );
+    }
+}
