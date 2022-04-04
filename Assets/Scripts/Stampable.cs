7469d41ee6e63ad36d338c9fe52207bc1d870fc4
Steven VD <steven@vandorp.lu>
2022-04-04 13:58:48 +0200

    Added scale mod to stampable

--- Assets/Scripts/Stampable.cs
+++ Assets/Scripts/Stampable.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class Stampable : MonoBehaviour {
 
     public Transform tfStampImageParent;
+    public float scaleMod = 1.0f;
 
     public virtual void Stamp(GameObject pfStamp, Vector2 absolutePosition, Quaternion rotation) {
-        GameObject.Instantiate(pfStamp, new Vector3(absolutePosition.x, absolutePosition.y, transform.position.z), rotation, tfStampImageParent);
+        GameObject stamp = GameObject.Instantiate(pfStamp, new Vector3(absolutePosition.x, absolutePosition.y, transform.position.z), rotation, tfStampImageParent);
+        stamp.transform.localScale *= scaleMod;
     }
 
 }
